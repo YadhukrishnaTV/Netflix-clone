518dacaff67c8aba92fd04d434e5b4ff1d0e87d8
YadhukrishnaTV <yadhukrishnatv@gmail.com>
2023-09-29 10:25:56 +0530

    clone

---
+++ netflix_b3/lib/utils/database/database_Images.dart
@@ -0,0 +1,52 @@
+class DataBaseImage {
+  static List gridImages = [
+    "assets/images/Rectangle 1.png",
+    "assets/images/Rectangle 3.png",
+    "assets/images/Rectangle 4.png",
+    "assets/images/Rectangle 5.png",
+    // "assets/images/Rectangle 5.png",
+    "assets/images/add.png"
+  ];
+}
+
+class DataB {
+  static List<Map> userimg=[
+    {"image":"assets/images/Rectangle 1.png","name":"emelano"},
+    {"image":"assets/images/Rectangle 4.png","name":"onyeka"},
+    {"image":"assets/images/Rectangle 3.png","name":"thelma"},
+   {"image":"assets/images/Rectangle 5.png","name":"Kids"},
+   {"image":"assets/images/add.png","name":"Add"},
+    
+
+  ];
+}
+
+class Name {
+  static List name = [
+    "emelano",
+    "onyeka",
+    "thelma",
+    "Kids",
+   
+    "Add",
+  ];
+}
+
+List movies = [
+  "assets/images/movies/1.png",
+  "assets/images/movies/2.png",
+  "assets/images/movies/3.png",
+  "assets/images/movies/4.png",
+  "assets/images/movies/5.png",
+  "assets/images/movies/6.png",
+  "assets/images/movies/7.png"
+];
+
+class Database1 {
+  static List userimg1=[
+    "assets/images/cursole/36 days of type & Poster everyday _ Poster Collection.jpeg",
+    "assets/images/cursole/Alternative Movie Posters vol_2.jpeg",
+    "assets/images/cursole/Concept Art Projects.jpeg",
+    "assets/images/cursole/Pirates of the Carribbean 4 - One Sheet #2 Poster and Poster Mount Bundle.jpeg"
+  ];
+}
